145278031c6faeb26a114013f3c4f00114a761ee
mondenel <mondenel1996@gmail.com>
2026-04-28 03:16:18 +0200

    Rewrite SearchBar.jsx to fix import error

--- kq/src/components/SearchBar.jsx
+++ kq/src/components/SearchBar.jsx
@@ -1,9 +1,4 @@
-import { useContext } from 'react'
-import { SearchContext } from '../pages/Home' // we'll lift search state later
-
 export default function SearchBar() {
-  // For simplicity, we'll manage search state in Home and pass it down.
-  // We'll implement by lifting SearchContext. For now placeholder.
   return (
     <div className="px-4 pb-3 bg-white border-b border-border-light">
       <div className="flex items-center bg-cream rounded-md px-3 py-2 gap-2">
